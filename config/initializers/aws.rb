f557f40f530554347f1e1f6dbb7afa7e8a1d5ab7
Jesse Farmer <jessethebuilder@yahoo.com>
2015-06-03 18:40:59 -0700

    Initial commit

---
+++ config/initializers/aws.rb
@@ -0,0 +1,10 @@
+CarrierWave.configure do |c|
+  c.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET'],
+    region: 'us-west-2'
+  }
+
+  c.fog_directory = ENV['AWS_BUCKET']
+end
