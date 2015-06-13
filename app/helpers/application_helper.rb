41d67e233674547e6a6b6e294a93e447f167d42c
Jesse Farmer <jessethebuilder@yahoo.com>
2015-06-12 22:28:31 -0700

    tweet widget

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,9 @@ module ApplicationHelper
   def user_is_this_dj?(dj)
     user_signed_in? && current_user.dj == dj
   end
+
+  def recent_tweet_widget
+    #todo
+    '<a class="twitter-timeline" href="https://twitter.com/killergreenrad1" data-widget-id="609589667843805184">Tweets by @killergreenrad1</a>'.html_safe
+  end
 end
